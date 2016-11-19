c5ecc89d822e35caf257c5dcac66d2334eaeb0a3
Sim <sim@Simui-MacBook-Pro.local>
2016-11-19 11:46:56 +0900

    init

---
+++ Sources/App/main.swift
@@ -0,0 +1,32 @@
+import Vapor
+import VaporMySQL
+
+do {
+    let driver = try MySQLDriver(
+        host: "127.0.0.1",
+        user: "root",
+        password: "Shj0811*^^*",
+        database: "tennis_development",
+        port: 3306,
+        flag: 0,
+        encoding: "utf8"
+    )
+    Database.default = Database(driver)
+} catch {
+    print("Could not initialize driver: \(error)")
+}
+
+let drop = Droplet(
+    database: Database.default,
+    preparations: [Post.self]
+)
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
